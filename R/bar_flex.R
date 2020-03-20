15164d5c7414414f8698f075b02b09d842f788fe
Vincent van Hees <vincentvanhees@gmail.com>
2020-03-20 18:08:34 +0100

    comment edit

--- R/bar_flex.R
+++ R/bar_flex.R
@@ -10,8 +10,8 @@
 # rewrite teh sequencing
 bar_flex<- function(bouts_values,bouts_lengths,f, bts){
   BLcopy = bouts_lengths
-  btss=bts*f # probably better to simply ask for btss as impute
-  # change lengths by 4 classes:
+  btss=bts*f # probably better to simply ask for btss as input
+  # change lengths to 1 of 4 classes:
   bouts_lengths[which(BLcopy >= btss[1] & BLcopy < btss[2])] = 1
   bouts_lengths[which(BLcopy >= btss[2] & BLcopy < btss[3])] = 2
   bouts_lengths[which(BLcopy >= btss[3] & BLcopy < btss[4])] = 3
