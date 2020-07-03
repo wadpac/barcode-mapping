44c336cdc7d16e64a5704a8e33db406de7541738
Vincent van Hees <vincentvanhees@gmail.com>
2020-07-03 15:07:52 +0200

    minor edits

--- R/shorting.barcode.R
+++ R/shorting.barcode.R
@@ -9,7 +9,7 @@
 shorting.barcode<-function(short_barcoding, barcode_per_day){
   # the trick.we assume the new sequencing is less than 1440. 
   # Actually, it shoud be as 1440*f in worst case. However, 1440 is far far enough
-  tt=rep(0,1440)
+  tt=rep(NA,1440)
   if (length(barcode_per_day) > 1440) {
     tt=barcode_per_day[1:1440]
   } else {
